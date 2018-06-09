7f42fd710974e76ead08f004c0563c1d3cf5feba
ablb7 <ablb7@aim.omaha.lib.ne.us>
2018-06-09 12:42:52 -0500

    Button Testing

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour {
+    [SerializeField] private Button _button1;
+    [SerializeField] private Button _button2;
+    void Start () {
+        _button1.onClick.AddListener(Test);
+	}
+	
+		
+	private void Test()
+    {
+        Debug.Log("1");
+    }
+}
