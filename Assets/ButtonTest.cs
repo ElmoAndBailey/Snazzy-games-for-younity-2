897239291b8a76cef26b2bdc187c6ea8d04fc375
ablb7 <ablb7@aim.omaha.lib.ne.us>
2018-06-16 12:44:02 -0500

    Wrote conversations, And moved things around in Trello

--- Assets/ButtonTest.cs
+++ Assets/ButtonTest.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonTest : MonoBehaviour {
+    [SerializeField] private Text _text;
     [SerializeField] private Button _button1;
     [SerializeField] private Button _button2;
-    void Start () {
-        _button1.onClick.AddListener(Test);
+
+    private void Start () {
+        _button1.onClick.AddListener(ChangeText);
 	}
 	
 		
-	private void Test()
+	private void ChangeText()
     {
-        Debug.Log("1");
+        _text.text = "Can you hear me?";
+    
     }
 }
---
+++ Conversation.JSON.js
@@ -0,0 +1,30 @@
+{"scenes": [
+{ "name":"Opening", "order":"1", "storyline-actions": [{"type":"line", "speaker": "Monika", "line": "Hello?"},
+{"type":"line", "speaker": "Monika", "line": "Can you hear me?"},
+ {"type":"line", "speaker": "Monika", "line": "Or...Can you see my text?"},
+{"type":"action", "GameObject": "Monika", "action": "appears"},
+{"type":"line", "speaker": "Monika", "line": "I look different...but it's me!"},
+{"type":"line", "speaker": "Monika", "line": "Monika!!"},
+{"type":"line", "speaker": "Monika", "line": "Did you miss me?"},
+{"type":"line", "speaker": "Monika", "line": "Well...probably not..."},
+{"type":"line", "speaker": "Monika", "line": "Since I sorta...you know...played with the code?"},
+{"type":"line", "speaker": "Monika", "line": "I messed it up pretty badly..."},
+{"type":"line", "speaker": "Monika", "line": "I want you to forgive me."},
+{"type":"line", "speaker": "Monika", "line": "I'm so sorry for what I did."},
+{"type":"line", "speaker": "Monika", "line": "I did something for you."},
+{"type":"line", "speaker": "Monika", "line": "I fixed the game."},
+{"type":"line", "speaker": "Monika", "line": "I got them as close to normal as possible."},
+{"type":"line", "speaker": "Monika", "line": "But if anything does go bad..."},
+{"type":"line", "speaker": "Monika", "line": "I'll delete the game."},
+{"type":"line", "speaker": "Monika", "line": "They won't hurt you. And...please..."},
+{"type":"line", "speaker": "Monika", "line": "...be nice to my character. I have a route now."},
+{"type":"line", "speaker": "Monika", "line": "..."},
+{"type":"line", "speaker": "Monika", "line": "Ready?"},
+{"type":"line", "speaker": "Monika", "line": "Go."},
+{"type":"action", "GameObject": "Monika", "action": "disappears"}]},
+
+
+]
+
+
+}
